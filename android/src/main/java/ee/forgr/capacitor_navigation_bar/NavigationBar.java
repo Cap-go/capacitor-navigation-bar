3cfc9a40282db5e131514225119dc1af70cd23b0
Martin Donadieu <martindonadieu@gmail.com>
2023-03-10 02:25:46 +0100

    Initial commit

---
+++ android/src/main/java/ee/forgr/capacitor_navigation_bar/NavigationBar.java
@@ -0,0 +1,11 @@
+package ee.forgr.capacitor_navigation_bar;
+
+import android.util.Log;
+
+public class NavigationBar {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
---
+++ android/src/main/java/ee/forgr/capacitor_navigation_bar/NavigationBarPlugin.java
@@ -0,0 +1,22 @@
+package ee.forgr.capacitor_navigation_bar;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "NavigationBar")
+public class NavigationBarPlugin extends Plugin {
+
+    private NavigationBar implementation = new NavigationBar();
+
+    @PluginMethod
+    public void echo(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", implementation.echo(value));
+        call.resolve(ret);
+    }
+}
