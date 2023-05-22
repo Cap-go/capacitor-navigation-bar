6ab7f88215fc72e8e6bba783904c7611a6e22aba
Martin Donadieu <martindonadieu@gmail.com>
2023-05-22 22:55:47 +0100

    fix: build issue

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ export default {
     {
       file: "dist/plugin.js",
       format: "iife",
-      name: "capacitorNavigationBar",
+      name: "capacitorCapacitorUpdater",
       globals: {
         "@capacitor/core": "capacitorExports",
       },
