32ef0f2b30a206050f00e71e1b8a9d4e58907569
Martin Donadieu <martindonadieu@gmail.com>
2025-02-09 04:54:42 +0000

    fix: missing class

--- ios/Plugin/NavigationBarPlugin.swift
+++ ios/Plugin/NavigationBarPlugin.swift
@@ -6,7 +6,7 @@ import Capacitor
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(NavigationBarPlugin)
-public class NavigationBarPlugin: CAPPlugin {
+public class NavigationBarPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "NavigationBarPlugin"
     public let jsName = "NavigationBar"
     public let pluginMethods: [CAPPluginMethod] = [
