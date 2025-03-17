12373e3625f0a3e24a9d330d679b557a4101dafe
Aerin Gamaroff <aerin@centbee.com>
2025-03-17 16:11:45 +0200

    lint fixes

--- android/src/main/java/ee/forgr/capacitor_navigation_bar/NavigationBarPlugin.java
+++ android/src/main/java/ee/forgr/capacitor_navigation_bar/NavigationBarPlugin.java
@@ -54,32 +54,37 @@ public class NavigationBarPlugin extends Plugin {
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-              WindowInsetsController insetsController = getActivity().getWindow().getInsetsController();
-                if (insetsController != null) {
-                    if (buttonStyle.equals("#000000")) {
-                        insetsController.setSystemBarsAppearance(
-                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                        );
-                    } else {
-                        insetsController.setSystemBarsAppearance(
-                            0,
-                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                        );
-                    }
-                }
-            } else {
-                // Fallback for devices running below Android 11
-                int flags = getActivity()
-                        .getWindow()
-                        .getDecorView()
-                        .getSystemUiVisibility();
+              WindowInsetsController insetsController = getActivity()
+                .getWindow()
+                .getInsetsController();
+              if (insetsController != null) {
                 if (buttonStyle.equals("#000000")) {
-                    flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+                  insetsController.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+                  );
                 } else {
-                    flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+                  insetsController.setSystemBarsAppearance(
+                    0,
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+                  );
                 }
-                getActivity().getWindow().getDecorView().setSystemUiVisibility(flags);
+              }
+            } else {
+              // Fallback for devices running below Android 11
+              int flags = getActivity()
+                .getWindow()
+                .getDecorView()
+                .getSystemUiVisibility();
+              if (buttonStyle.equals("#000000")) {
+                flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+              } else {
+                flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+              }
+              getActivity()
+                .getWindow()
+                .getDecorView()
+                .setSystemUiVisibility(flags);
             }
           }
           call.resolve();
