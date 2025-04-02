de8dc73d7a95af69fef5d7c512c76ab126396a2c
Tomasz Chomicz <tomasz.chomicz@appyourself.net>
2025-04-02 13:18:16 +0200

    fix build

--- src/web.ts
+++ src/web.ts
@@ -1,14 +1,15 @@
 import { WebPlugin } from "@capacitor/core";
 
-import type { NavigationBarPlugin } from "./definitions";
+import type { NavigationBarPlugin} from "./definitions";
+import { NavigationBarButtonStyle, NavigationBarColor } from "./definitions";
 
 export class NavigationBarWeb extends WebPlugin implements NavigationBarPlugin {
-  async setNavigationBarColor(options: { color: string }): Promise<void> {
+  async setNavigationBarColor(options: { color: string; buttonStyle: NavigationBarButtonStyle}): Promise<void> {
     console.log("Cannot setNavigationBarColor on web", options);
     return;
   }
-  async getNavigationBarColor(): Promise<{ color: string }> {
+  async getNavigationBarColor(): Promise<{ color: string; buttonStyle: NavigationBarButtonStyle }> {
     console.log("Cannot getNavigationBarColor on web");
-    return { color: "#000000" };
+    return { color: NavigationBarColor.BLACK, buttonStyle: NavigationBarButtonStyle.LIGHT };
   }
 }
