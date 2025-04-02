945a8f075e6d552a063d8e753d6fd50de0aceb27
Tomasz Chomicz <tomasz.chomicz@appyourself.net>
2025-04-02 13:51:19 +0200

    fix build

--- src/web.ts
+++ src/web.ts
@@ -4,7 +4,7 @@ import type { NavigationBarPlugin} from "./definitions";
 import { NavigationBarButtonStyle, NavigationBarColor } from "./definitions";
 
 export class NavigationBarWeb extends WebPlugin implements NavigationBarPlugin {
-  async setNavigationBarColor(options: { color: string; buttonStyle: NavigationBarButtonStyle}): Promise<void> {
+  async setNavigationBarColor(options: { color: string; buttonStyle?: NavigationBarButtonStyle}): Promise<void> {
     console.log("Cannot setNavigationBarColor on web", options);
     return;
   }
