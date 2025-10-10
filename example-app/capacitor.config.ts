05cd50e0e298cda660378bd7d02df979e792a2a9
Martin Donadieu <martindonadieu@gmail.com>
2025-10-10 04:53:56 +0100

    chore: rename app

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "app.capgo.navigation.demo",
+  appId: "app.capgo.navigation.bar",
   appName: "Navigation Bar Demo",
   webDir: "dist",
   bundledWebRuntime: false,
