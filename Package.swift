dae42774ab4d155fa7c3c831475eb0c4314e75d8
Martin Donadieu <martindonadieu@gmail.com>
2025-11-17 10:59:41 +0000

    fix: fuck off renovate stopbumping that

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.4.4")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
     ],
     targets: [
         .target(
